4924df324189241607bdd83bf5333d6191d2e3d2
Allan-Sanchez <asanchezrixtun@gmail.com>
2020-07-02 15:50:48 -0600

    get data list name

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,8 @@ module.exports = {
     extend: {},
   },
   variants: {},
-  plugins: [],
+  // plugins: [require("@tailwindcss/custom-forms")]
+  plugins: [
+    require('@tailwindcss/custom-forms')
+  ]
 }
